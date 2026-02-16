6a105db86ada445ee372e04d8ca2bd1d232c4f94
Thiago <thiago.pereira.moreira@gmail.com>
2026-02-15 21:58:12 -0300

    test

---
+++ open-next.config.ts
@@ -0,0 +1,8 @@
+import { defineCloudflareConfig } from '@opennextjs/cloudflare'
+import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache'
+import { withRegionalCache } from '@opennextjs/cloudflare/overrides/incremental-cache/regional-cache'
+import doQueue from '@opennextjs/cloudflare/overrides/queue/do-queue'
+import doShardedTagCache from '@opennextjs/cloudflare/overrides/tag-cache/do-sharded-tag-cache'
+import { purgeCache } from '@opennextjs/cloudflare/overrides/cache-purge/index'
+
+export default defineCloudflareConfig();
